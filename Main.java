9ba373a5cc77518b932f4fc6006d0b00be5c02f7
Hywel <bonnefondhywel@gmail.com>
2023-01-16 16:49:20 +0100

    begin_grille

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Grille g1 = new Grille(6, 7);
+        g1.afficher();
+
+    }
+
+}
---
+++ Plateau.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Plateau {
+    
+    private int nbLig;
+    private int nbCol;
+    private char [][] grille;
+
+    public Grille(int n, int p){
+        nbLig = n;
+        nbCol = p;
+        grille = new char[nbLig][nbCol];
+
+        for(int i=0; i<nbLig; i++){
+            for(int j=0; j<nbCol; j++){
+                grille[i][j] = ' ';
+            }
+        }
+    }
+
+    public void afficher(){
+        System.out.println();
+        for(int i=0; i<nbLig; i++){
+            for(int j=0; j<nbCol; j++){
+                System.out.println(" | " + grille[i][j]);
+            }
+            System.out.println(" | ");
+        }
+        System.out.println();
+    }
+
+}
