91ae40d4f2e1d902e6b7493a782c6a606b56d72a
Hywel <bonnefondhywel@gmail.com>
2023-01-16 15:12:35 +0100

    finish_menu

--- menu.java
+++ menu.java
@@ -1,19 +1,20 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class menu {
+public class Menu {
+    
     public static void printMenu(String[] options){
         for (String option : options){
             System.out.println(option);
         }
-        System.out.print("Bienvenue au jeu du puissance 4 !");
-        System.out.print("Veullez choisir une option parmi les quatre proposés, s'il vous plaît : ");
+        System.out.println("Bienvenue au jeu du puissance 4 !\n");
+        System.out.print("Veullez choisir une option parmi les quatre proposés en tapant le bon chiffre, s'il vous plaît : ");
     }
     public static void main(String[] args) {
-        String[] options = {"1- Affronter l'ordinateur",
+        String[] options = {"\n1- Affronter l'ordinateur",
                             "2- Affronter un joueur",
                             "3- Regarder les meilleures scores",
-                            "4- Quitter",
+                            "4- Quitter\n",
         };
         try (Scanner scanner = new Scanner(System.in)) {
             int option = 1;
@@ -21,19 +22,26 @@ public class menu {
                 printMenu(options);
                 try {
                     option = scanner.nextInt();
-                    //if (option <= 1 ) {
-                        
-                    //}
+                    if (option < 1 || option > 4) {
+                        System.out.println("\nS'il vous plaît, entrez un chiffre compris entre 1 et 4.");
+                    } else if(option == 1){
+                        System.out.println("\nVous avez choisi l'option 1.");
+                    } else if(option == 2){
+                        System.out.println("\nVous avez choisi l'option 2.");
+                    } else if(option == 3){
+                        System.out.println("\nVous avez choisi l'option 3.");
+                    }
                 }
                 catch (InputMismatchException ex){
-                    System.out.println("Please enter an integer value between 1 and " + options.length);
+                    System.out.println("\nS'il vous plaît, entrez un chiffre compris entre 1 et 4.");
                     scanner.next();
                 }
                 catch (Exception ex){
-                    System.out.println("An unexpected error happened. Please try again");
+                    System.out.println("\nUne erreur inconnue est apparue. Réessayer encore, s'il vous plaît.");
                     scanner.next();
                 }
             }
+            System.out.println("\nMerci d'avoir utilisé notre programme. Nous vous souhaitons une excellente journée.\n");
         }
     }
 }
