1054ab8551b771c24f077c6191b4923d86ee3988
Hywel <bonnefondhywel@gmail.com>
2023-01-17 16:29:39 +0100

    its_difficult

---
+++ Puissance4/Class/Victoire.java
@@ -0,0 +1,53 @@
+package Puissance4.Class;
+
+public class Victoire {
+
+    public static boolean isWinner(char player, char[][] grid){
+		//check for 4 across
+		for(int row = 0; row<grid.length; row++){
+			for (int col = 0;col < grid[0].length - 3;col++){
+				if (grid[row][col] == player   && 
+					grid[row][col+1] == player &&
+					grid[row][col+2] == player &&
+					grid[row][col+3] == player){
+					return true;
+				}
+			}			
+		}
+		//check for 4 up and down
+		for(int row = 0; row < grid.length - 3; row++){
+			for(int col = 0; col < grid[0].length; col++){
+				if (grid[row][col] == player   && 
+					grid[row+1][col] == player &&
+					grid[row+2][col] == player &&
+					grid[row+3][col] == player){
+					return true;
+				}
+			}
+		}
+		//check upward diagonal
+		for(int row = 3; row < grid.length; row++){
+			for(int col = 0; col < grid[0].length - 3; col++){
+				if (grid[row][col] == player   && 
+					grid[row-1][col+1] == player &&
+					grid[row-2][col+2] == player &&
+					grid[row-3][col+3] == player){
+					return true;
+				}
+			}
+		}
+		//check downward diagonal
+		for(int row = 0; row < grid.length - 3; row++){
+			for(int col = 0; col < grid[0].length - 3; col++){
+				if (grid[row][col] == player   && 
+					grid[row+1][col+1] == player &&
+					grid[row+2][col+2] == player &&
+					grid[row+3][col+3] == player){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+    
+}
--- Puissance4/Main.java
+++ Puissance4/Main.java
@@ -1,7 +1,5 @@
 package Puissance4;
 
-import Puissance4.Class.Plateau;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -21,41 +19,45 @@ public class Main {
                             "4- Quitter\n",
         };
         try (Scanner scanner = new Scanner(System.in)) {
-            int option = 1;
-            while (option!=4){
+            int option = 5;
+            while (true){
                 printMenu(options);
                 try {
                     option = scanner.nextInt();
                     if (option < 1 || option > 4) {
                         System.out.println("\nS'il vous plaît, entrez un chiffre compris entre 1 et 4.");
+                        continue;
                     } else if(option == 1){
-                        try (Scanner scan = new Scanner(System.in)) {
-                            System.out.println("\nJoueur 1, choisissez votre pseudo\n");
-                            String player1Name = scan.nextLine();
-                            System.out.println("\nJoueur 1, choisissez votre symbole\n");
-                            char player1Symbol = scan.nextLine().charAt(0);
-                            System.out.println("\nJoueur 2, choisissez votre pseudo\n");
-                            String player2Name = scan.nextLine();
-                            System.out.println("\nJoueur 2, choisissez votre symbole\n");
-                            char player2Symbol = scan.nextLine().charAt(0);
-                        }
-                        Plateau.gridScreen(7, 6);
+                        System.out.println("\nVous avez choisi l'option 1.");
+                        continue;
                     } else if(option == 2){
-                        System.out.println("\nVous avez choisi l'option 2.");
+                        break;
                     } else if(option == 3){
                         System.out.println("\nVous avez choisi l'option 3.");
+                        continue;
+                    } else if(option == 4){
+                        System.out.println("\nMerci d'avoir utilisé notre programme ! Nous vous souhaitons une excellente journée !\n");
+                        System.exit(0);
                     }
                 }
                 catch (InputMismatchException ex){
                     System.out.println("\nS'il vous plaît, entrez un chiffre compris entre 1 et 4.");
                     scanner.next();
                 }
-                catch (Exception ex){
-                    System.out.println("\nUne erreur inconnue est apparue. Réessayez encore, s'il vous plaît.");
-                    scanner.next();
-                }
             }
-            System.out.println("\nMerci d'avoir utilisé notre programme ! Nous vous souhaitons une excellente journée. !\n");
+            while(true){
+                try (Scanner scan = new Scanner(System.in)) {
+                    System.out.println("\nJoueur 1, choisissez votre pseudo\n");
+                    String player1Name = scan.nextLine();
+                    System.out.println("\nJoueur 1, choisissez votre symbole\n");
+                    char player1Symbol = scan.nextLine().charAt(0);
+                    System.out.println("\nJoueur 2, choisissez votre pseudo\n");
+                    String player2Name = scan.nextLine();
+                    System.out.println("\nJoueur 2, choisissez votre symbole\n");
+                    char player2Symbol = scan.nextLine().charAt(0);
+                }
+                break;
+            }
         }
     }
 }
